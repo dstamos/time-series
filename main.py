890767004cbd9e02c357d36e1d2ac5070aa42029
dstamos <dimitrios.i.stamos@gmail.com>
2020-09-01 12:29:28 +0100

    Xgboost added.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.data_management import DataHandler, Settings
-from src.training import Sarimax
+from src.training import Sarimax, Xgboost
 
 
 def main():
@@ -18,18 +18,28 @@ def main():
     model = Sarimax(settings)
     model.fit(data)
 
-    model.forecast(data, period=20*30)
+    forecast_period = 24*7
+    model.forecast(data, period=forecast_period)
+    #
+    model_xgboost = Xgboost(settings)
+    model_xgboost.fit(data)
+
+    forecast_period = 24*7
+    model_xgboost.forecast(data, period=forecast_period)
+    #
 
     import matplotlib.pyplot as plt
 
     plt.figure()
     plt.plot(data.labels.iloc[:settings.data.horizon*settings.data.n_horizons_lookback], 'k')
-    plt.plot(data.labels.iloc[settings.data.horizon*settings.data.n_horizons_lookback:settings.data.horizon*settings.data.n_horizons_lookback+20*30], 'tab:blue')
+    plt.plot(data.labels.iloc[settings.data.horizon*settings.data.n_horizons_lookback:settings.data.horizon*settings.data.n_horizons_lookback+forecast_period], 'tab:blue')
 
     plt.plot(model.prediction, 'tab:red')
+    plt.plot(model_xgboost.prediction, 'tab:orange')
 
     plt.pause(0.1)
     plt.show()
+    print('done')
 
     k = 1
 
@@ -45,10 +55,19 @@ def main():
     # results = pickle.load(open('results/' + str(settings.data.dataset) + '.pckl', "rb"))
 
     # TODO Try SARIMAX without diff on indicators
+    # TODO Try predicting the average co2 of the rolling 8 hours
     # TODO xgboost for prediction
-    # TODO Add one-hot encoding of hourly time features for xgboost and don't lag them
+    #   TODO Add one-hot encoding of hourly time features for xgboost and don't lag them
     # TODO NBeats for prediction
     # TODO Rework all classes in training.py to match the "model" superclass in pytorch
+
+    # TODO Testing metrics
+
+    # TODO Simple linear regression (AR(p))
+    # TODO Work on the diff of the labels and recover the full label afterwards
+    # TODO Multivariate NBEATS
+
+    # TODO Read NBEATS experiments metalearning
 
 
 if __name__ == "__main__":
--- src/training.py
+++ src/training.py
@@ -1,13 +1,15 @@
 import numpy as np
 import pandas as pd
+import xgboost
+import multiprocess as mp
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 
 
 class Sarimax:
     def __init__(self, settings):
         self.settings = settings
-        self.ar_order = 5
-        self.difference_order = 1
+        self.ar_order = 2
+        self.difference_order = 0
         self.ma_order = 1
 
         self.seasonal_ar_order = 1
@@ -28,7 +30,7 @@ class Sarimax:
         model_exog = SARIMAX(data.labels.iloc[:horizon*n_horizons_lookback], exog=exog_variables,
                              order=(self.ar_order, self.difference_order, self.ma_order),
                              seasonal_order=(self.seasonal_ar_order, self.seasonal_difference_order, self.seasonal_ma_order, self.seasonal_period))
-        self.model = model_exog.fit(disp=True, maxiter=50)
+        self.model = model_exog.fit(disp=True, maxiter=150)
 
     def forecast(self, data, period=1):
         horizon = self.settings.data.horizon
@@ -46,13 +48,88 @@ class Sarimax:
 class Xgboost:
     def __init__(self, settings):
         self.settings = settings
-        self.lags = 12
+        self.lags = 3
 
         self.model = None
         self.prediction = None
 
-    def fit(self):
-        pass
+    def fit(self, data):
+        horizon = self.settings.data.horizon
+        n_horizons_lookback = self.settings.data.n_horizons_lookback
+        labels = data.labels.iloc[:horizon*n_horizons_lookback]
+        if self.settings.training.use_exog is True:
+            x = data.features.iloc[:horizon*n_horizons_lookback].diff().fillna(method='bfill')
+            # x = self._time_features(x)
+            features = self._lag_features(x)
+            features, labels = self._prune_data(features, labels)
+        else:
+            raise ValueError('Featureless xgboost not implemented.')
+        import xgboost
+        dmatrix = xgboost.DMatrix(features, labels)
 
-    def forecast(self):
-        pass
+        # noinspection PyUnresolvedReferences
+        params = {'max_depth': 6,  # 6
+                  'colsample_bytree': 0.2,
+                  'eta': 0.05,
+                  'num_parallel_tree': 20,  # 20
+                  'n_jobs': -2,
+                  'obj': 'reg:squarederror',
+                  'verbosity': 0,
+                  'seed': 2130,  # TODO This seed is fixed at the moment
+                  # 'eval_metric': 'rmse',
+                  'booster': 'dart',  # gbtree
+                  'min_child_weight': 2,
+                  'nthread': mp.cpu_count()-1}
+        self.model = xgboost.train(params, dmatrix, num_boost_round=500, evals=[(dmatrix, "training")], verbose_eval=1)
+
+    def forecast(self, data, period=1):
+        horizon = self.settings.data.horizon
+        n_horizons_lookback = self.settings.data.n_horizons_lookback
+        if self.settings.training.use_exog is True:
+            x = data.features.iloc[horizon*n_horizons_lookback:horizon*n_horizons_lookback+period].diff().fillna(method='bfill')
+            # x = self._time_features(x)
+            features = self._lag_features(x)
+            features = self._prune_data(features)
+        else:
+            raise ValueError('Featureless xgboost not implemented.')
+
+        self.prediction = pd.DataFrame(self.model.predict(xgboost.DMatrix(features)), index=features.index)
+
+    @staticmethod
+    def _time_features(indicators):
+        datetimes = indicators.index
+
+        times = datetimes.hour
+
+        F = np.zeros((len(times), 24))
+        F[range(len(times)), times] = 1
+        for idx in range(24):
+            indicators['calendar_' + str(idx)] = F[:, idx]
+        return indicators
+
+    def _lag_features(self, indicators):
+        lagged_indicators = [None] * self.lags
+        original_column_names_to_lag = indicators.columns.values.tolist()
+        original_column_names_to_lag = [s for s in original_column_names_to_lag if 'calendar' not in s]     # for selective lags
+        for idx, c_lag in enumerate(range(self.lags)):
+            column_names = [column_name + '-lagged_' + str(c_lag) for column_name in original_column_names_to_lag]
+            temp = indicators[original_column_names_to_lag].shift(c_lag)
+            temp.columns = column_names
+            lagged_indicators[idx] = temp
+
+        lagged_indicators.insert(0, indicators)
+        lagged_indicators = pd.concat(lagged_indicators, axis=1)
+
+        return lagged_indicators
+
+    @ staticmethod
+    def _prune_data(features, labels=None):
+        features = features.replace([np.inf, -np.inf], np.nan)
+        indexes_to_drop = features.index[pd.isnull(features).any(1).to_numpy().nonzero()[0]]
+
+        features = features.drop(indexes_to_drop)
+        if labels is not None:
+            labels = labels.drop(indexes_to_drop)
+            return features, labels
+        else:
+            return features
