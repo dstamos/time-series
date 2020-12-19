Fixed the average "bug".

main.py
@@ -24,7 +24,7 @@ def main(curr_seed, n_tr_points):
                      'test_tasks_pct': 0.2,
                      'n_tr_points': n_tr_points,
                      'n_test_points': 2 * 24 * 365,
-                     'forecast_length': 24}
+                     'forecast_length': 1}
 
     # data_settings = {'dataset': 'synthetic_ar',
     #                  'use_exog': False,
@@ -44,8 +44,8 @@ def main(curr_seed, n_tr_points):
     data = MealearningDataHandler(data_settings)
     training_settings = Settings({'method': 'ITL',
                                   'use_exog': False,
-                                  'regularization_parameter_range': [10 ** float(i) for i in np.linspace(-6, 6, 24)],
-                                  'lags': 2,
+                                  'regularization_parameter_range': [10 ** float(i) for i in np.linspace(-12, 6, 36)],
+                                  'lags': 8,
                                   'horizon': data_settings.forecast_length})
 
     model_itl = training(data, training_settings)
@@ -62,8 +62,8 @@ def main(curr_seed, n_tr_points):
     data = MealearningDataHandler(data_settings)
     training_settings = Settings({'method': 'BiasLTL',
                                   'use_exog': False,
-                                  'regularization_parameter_range': [10 ** float(i) for i in np.linspace(-6, 6, 24)],
-                                  'lags': 2,
+                                  'regularization_parameter_range': [10 ** float(i) for i in np.linspace(-12, 6, 36)],
+                                  'lags': 8,
                                   'horizon': data_settings.forecast_length})
 
     model_ltl = training(data, training_settings)
@@ -97,28 +97,18 @@ def main(curr_seed, n_tr_points):
     my_dpi = 100
     n_plots = min(len(data.test_tasks), 8)
     fig, ax = plt.subplots(figsize=(2 * 1920 / my_dpi, 2 * 1080 / my_dpi), facecolor='white', dpi=my_dpi, nrows=n_plots, ncols=1)
+    lookback = 24 * 21
     for task_idx in range(n_plots):
         curr_ax = ax[task_idx]
-        # true = data.test_tasks[task_idx].test.raw_time_series
-        # ax.plot(true, color='k', label='original time series')
+        true = data.test_tasks[task_idx].test.labels.iloc[-lookback:]
+        curr_ax.plot(true, color='k', label='original labels')
 
-        pred_itl = model_itl.all_raw_predictions[task_idx].iloc[-24 * 21:]
+        pred_itl = model_itl.all_predictions[task_idx].iloc[-lookback:]
         curr_ax.plot(pred_itl, color='tab:red', label='Independent Learning')
 
-        # ax.plot(model_sarimax.all_predictions[task_idx], color='tab:red', label='SARIMAX predictions')
-        # ax.plot(model_sarimax.all_forecasts[task_idx], color='tab:orange', label='SARIMAX forecasts')
-
-        # ax.plot(model_xgboost.all_raw_predictions[task_idx], color='tab:green', label='Random Forest')
-
-        pred = model_ltl.all_raw_predictions[task_idx].iloc[-24 * 21:]
-        pred = pred.loc[pred_itl.index]
-        true = pd.DataFrame(data.test_tasks[task_idx].test.raw_time_series, index=data.test_tasks[task_idx].test.raw_time_series.index)
-        true = true.loc[pred.index].iloc[-24 * 21:]
-        curr_ax.plot(true, color='k', label='Original')
-        pred = pd.DataFrame(np.mean([pred.values, pred.values, true.values.ravel(), true.values.ravel(), true.values.ravel(), true.values.ravel()], axis=0), index=pred.index)
+        pred = model_ltl.all_predictions[task_idx].iloc[-lookback:]
         curr_ax.plot(pred, color='tab:blue', label='Bias Meta-learning')
 
-        # curr_ax.set_ylim([np.min(true.values), np.max(true.values)])
         curr_ax.axhline(y=0, color='tab:gray', linestyle=':')
         curr_ax.spines["top"].set_visible(False)
         curr_ax.spines["right"].set_visible(False)
@@ -138,18 +128,13 @@ if __name__ == "__main__":
         tr_points_range = [int(sys.argv[2])]
     else:
         seed_range = [999]
-        tr_points_range = [24 * 14]
+        tr_points_range = [24 * 4]
         # seed_range = range(1, 21)
         # tr_points_range = [24 * 7, 24 * 30, 24 * 90, 24 * 365]
-
-    # seed_range = [1]
-    # tr_points_range = [24 * 30]
 
     # seed_range = range(1, 21)
     # tr_points_range = range(12, 201, 2)
 
-    # seed_range = range(1, 21)
-    # tr_points_range = range(12, 25, 2)
     tt = time()
     for seed in seed_range:
         for tr_points in tr_points_range:

post_processing_true_pred_plots.py
@@ -9,7 +9,7 @@ import pickle
 
 
 seed = 999
-tr_points = 24 * 4
+tr_points = 24 * 30
 tt = time()
 
 # Load the models
@@ -24,17 +24,16 @@ data = MealearningDataHandler(data_settings)
 my_dpi = 100
 n_plots = min(len(data.test_tasks), 8)
 fig, ax = plt.subplots(figsize=(2 * 1920 / my_dpi, 2 * 1080 / my_dpi), facecolor='white', dpi=my_dpi, nrows=n_plots, ncols=1)
+lookback = 24 * 90
 for task_idx in range(n_plots):
     curr_ax = ax[task_idx]
-    pred_itl = model_itl.all_raw_predictions[task_idx].iloc[-24 * 21:]
+    true = data.test_tasks[task_idx].test.labels.iloc[-lookback:]
+    curr_ax.plot(true, color='k', label='original labels')
+
+    pred_itl = model_itl.all_predictions[task_idx].iloc[-lookback:]
     curr_ax.plot(pred_itl, color='tab:red', label='Independent Learning')
 
-    pred = model_ltl.all_raw_predictions[task_idx].iloc[-24 * 21:]
-    pred = pred.loc[pred_itl.index]
-    true = pd.DataFrame(data.test_tasks[task_idx].test.raw_time_series, index=data.test_tasks[task_idx].test.raw_time_series.index)
-    true = true.loc[pred.index].iloc[-24 * 21:]
-    curr_ax.plot(true, color='k', label='Original')
-    pred = pd.DataFrame(np.mean([pred.values, pred.values, true.values.ravel(), true.values.ravel(), true.values.ravel(), true.values.ravel()], axis=0), index=pred.index)
+    pred = model_ltl.all_predictions[task_idx].iloc[-lookback:]
     curr_ax.plot(pred, color='tab:blue', label='Bias Meta-learning')
 
     curr_ax.axhline(y=0, color='tab:gray', linestyle=':')

src/data_management.py
@@ -481,32 +481,25 @@ class MealearningDataHandler:
     def air_quality_madrid(self):
         import pickle
 
-        measures = pickle.load(open('madrid_data.pckl', "rb"))
-
         station_info = pd.read_csv('./data/air_quality_madrid/stations.csv')
         station_info.index = station_info.id
         station_names = []
         for station_id in station_info.index:
             station_names.append(station_info.loc[int(station_id)]['name'])
 
-        # station_ids = measures['station'].unique().tolist()
         station_ids = station_info.index
-        split_datasets = []
-        for station_id in station_ids:
-            # Four major pollutants https://www.blf.org.uk/support-for-you/air-pollution/types
-            # O3 (Ground-level Ozone)
-            # PM10 (Particulate Matter (soot and dust))
-            # SO2 (Sulphur Dioxide)
-            # NO2 (Nitrogen Dioxide)
-            df = measures.loc[measures.station.astype(int) == station_id].drop(['station'], axis=1).astype(np.float)
-            df = df[['O_3', 'PM10', 'SO_2', 'NO_2']]
-            split_datasets.append(df)
-
-        # for idx in range(len(station_ids)):
-        #     print(split_datasets[idx].head())
-        #     print(split_datasets[idx].tail())
-        #     for i in range(5):
-        #         print('')
+        # split_datasets = []
+        # measures = pickle.load(open('madrid_data.pckl', "rb"))
+        # for station_id in station_ids:
+        #     # Four major pollutants https://www.blf.org.uk/support-for-you/air-pollution/types
+        #     # O3 (Ground-level Ozone)
+        #     # PM10 (Particulate Matter (soot and dust))
+        #     # SO2 (Sulphur Dioxide)
+        #     # NO2 (Nitrogen Dioxide)
+        #     df = measures.loc[measures.station.astype(int) == station_id].drop(['station'], axis=1).astype(np.float)
+        #     df = df[['O_3', 'PM10', 'SO_2', 'NO_2']]
+        #     split_datasets.append(df)
+        split_datasets = pickle.load(open('split_stations.pckl', "rb"))
 
         all_full_time_series = []
         for idx in range(len(station_ids)):

src/ltl.py
@@ -38,8 +38,8 @@ class BiasLTL:
                 x_train = training_tasks[task_idx].training.features.values
                 y_train = training_tasks[task_idx].training.labels.values.ravel()
 
-                mean_vector = self._solve_wrt_h(mean_vector, x_train, y_train, regularization_parameter, curr_iteration=task_idx, inner_iter_cap=10)
-                # print(mean_vector)
+                mean_vector = self._solve_wrt_h(mean_vector, x_train, y_train, regularization_parameter, curr_iteration=task_idx, inner_iter_cap=1)
+
                 all_average_vectors.append(mean_vector)
             #####################################################
             # Validation only needs to be measured at the very end, after we've trained on all training tasks
@@ -49,17 +49,12 @@ class BiasLTL:
                 x_val = validation_tasks[validation_task_idx].validation.features.values
                 y_val = validation_tasks[validation_task_idx].validation.labels
 
-                temp_best_val_perf = np.Inf
-                for temp_regul_param in self.settings.regularization_parameter_range:
-                    # for temp_regul_param in [regularization_parameter]:
-                    w = self._solve_wrt_w(mean_vector, x_train, y_train, temp_regul_param)
+                w = self._solve_wrt_w(mean_vector, x_train, y_train, regularization_parameter)
 
-                    curr_predictions = pd.Series(x_val @ w, index=y_val.index)
-                    errors = performance_check(y_val, curr_predictions)
-                    temp_val_perf = errors['nmse']
-                    if temp_val_perf < temp_best_val_perf:
-                        temp_best_val_perf = temp_val_perf
-                validation_performances.append(temp_best_val_perf)
+                curr_predictions = pd.Series(x_val @ w, index=y_val.index)
+                errors = performance_check(y_val, curr_predictions)
+                temp_val_perf = errors['nmse']
+                validation_performances.append(temp_val_perf)
             validation_performance = np.mean(validation_performances)
             print(f'LTL | lambda: {regularization_parameter:6e} | val performance: {validation_performance:12.5f} | {time() - tt:5.2f}sec')
 
@@ -111,9 +106,6 @@ class BiasLTL:
                         validation_criterion = False
 
                     if validation_criterion:
-                        print(regularization_parameter)
-                        print(validation_performance)
-                        print('')
                         best_val_performance = validation_performance
                         best_w = w
 
@@ -162,7 +154,7 @@ class BiasLTL:
             y_n_hat = 1 / np.sqrt(n) * (y - x @ lstsq(c_n_hat, x.T @ y)[0] / n)
 
             grad_h = x_n_hat.T @ (x_n_hat @ curr_h - y_n_hat)
-            grad_h = np.clip(grad_h, a_min=-10**10, a_max=10**10)
+            grad_h = np.clip(grad_h, a_min=-10**3, a_max=10**3)
             return grad_h
             # import warnings
             # with warnings.catch_warnings():